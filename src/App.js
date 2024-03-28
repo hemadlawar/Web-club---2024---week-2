fb6ccb50c16638e6a649394c4d6d8c1af089b993
hemaddlawar <hema.200219627512@gmail.com>
2024-03-28 17:45:18 +0300

    add kurdish language

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Weather from "./Weather";
 const App = () => {
   return (
     <div>
-      <h1>Weather Forecast App</h1>
       <Weather />
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,19 @@ import axios from "axios";
 const Weather = () => {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+
   const apiKey = "f0852da6f6c8c94a01272a1b4da07455";
+
+  const [titles, setTitles] = useState({
+    headers: "Weather Forecast App",
+    Get_Weather: "Get Weather",
+    Temperature: "Temperature",
+    Description: "Description",
+    Feels_like: "Feels like",
+    Humidity: "Humidity",
+    Pressure: "Pressure",
+    Wind_Speed: "Wind Speed",
+  });
   const fetchData = async () => {
     try {
       const response = await axios.get(
@@ -30,8 +42,41 @@ const Weather = () => {
     fetchData();
   };
 
+  const convertToKurdish = () => {
+    setTitles({
+      headers: "پلاتفۆرمی كه‌ش و هه‌وا",
+
+      Get_Weather: "زانینی كه‌ش و هه‌وا",
+      Temperature: "پله‌ی گه‌رمی",
+      Description: "وەسف",
+      Feels_like: "هه‌ست پێكردن",
+      Humidity: "شێ",
+      Pressure: "په‌ستانی هه‌وا",
+      Wind_Speed: "خێرایی با",
+    });
+  };
+
+  const convertToEnglish = () => {
+    setTitles({
+      headers: "Weather Forecast App",
+      Get_Weather: "Get Weather",
+      Temperature: "Temperature",
+      Description: "Description",
+      Feels_like: "Feels like",
+      Humidity: "Humidity",
+      Pressure: "Pressure",
+      Wind_Speed: "Wind Speed",
+    });
+  };
+
   return (
     <div>
+      <div>
+        {" "}
+        <button onClick={convertToKurdish}>كوردی </button>
+        <button onClick={convertToEnglish}>English </button>
+      </div>
+      <h1>{titles.headers}</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -39,17 +84,30 @@ const Weather = () => {
           value={city}
           onChange={handleInputChange}
         />
-        <button type="submit">Get Weather</button>
+        <button type="submit">{titles.Get_Weather}</button>
       </form>
       {weatherData ? (
         <>
           <h2>{weatherData.name}</h2>
-          <p>Temperature: {weatherData.main.temp}°C</p>
-          <p>Description: {weatherData.weather[0].description}</p>
-          <p>Feels like : {weatherData.main.feels_like}°C</p>
-          <p>Humidity : {weatherData.main.humidity}%</p>
-          <p>Pressure : {weatherData.main.pressure}</p>
-          <p>Wind Speed : {weatherData.wind.speed}m/s</p>
+          <p>
+            {titles.Temperature}: {weatherData.main.temp}°C
+          </p>
+          <p>
+            {titles.Description}: {weatherData.weather[0].description}
+          </p>
+          <p>
+            {titles.Feels_like} : {weatherData.main.feels_like}°C
+          </p>
+          <p>
+            {" "}
+            {titles.Humidity} : {weatherData.main.humidity}%
+          </p>
+          <p>
+            {titles.Pressure} : {weatherData.main.pressure}
+          </p>
+          <p>
+            {titles.Wind_Speed} : {weatherData.wind.speed}m/s
+          </p>
         </>
       ) : (
         <p>Loading weather data...</p>
